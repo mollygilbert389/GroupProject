0942045139005cb2e79989528dca73886208729f
Joel Fecke <jfecke@gmail.com>
2018-11-15 23:09:34 -0600

    everything runs fine, need to find way to return value

--- javascript/recommend.js
+++ javascript/recommend.js
@@ -3,24 +3,18 @@
 //person movie credits: "https://api.themoviedb.org/3/person/{person_id}/movie_credits?api_key=c5e11b07aed33fed93509604abbe325f&language=en-US"
 //movie database link: https://developers.themoviedb.org/3/movies/get-movie-details
 
-var favmovies = [];
-var imdbIDs = ["tt0172495","tt0095016","tt1431045","tt0112573","tt0371746","tt4481414"];
-var tmdbIDs = [];
+var favmovies = ["tt0172495","tt0095016","tt1431045","tt0112573","tt0371746","tt4481414"];
 var recomObj = {};
 var ratedObj = {};
 var recommendArray= [];
 var recommendations = [];
-var test = [];
-var recoObj = {};
 
-var getTMDBids = function(array) {
+var getRecommendations = async function(array) {
     var idArray = [];
     var promises = array.map( (id) => {
         return new Promise(function(res) {
-            
             var IDurl = "https://api.themoviedb.org/3/find/" + String(id) + "?api_key=c5e11b07aed33fed93509604abbe325f&language=en-US&external_source=imdb_id"
             var tmdbID = "";
-
             $.ajax({
                 url: IDurl,
                 method: "GET"
@@ -31,22 +25,16 @@ var getTMDBids = function(array) {
             })
         })
     })
-
     Promise.all(promises).then(function(alldata) {
-        console.log(alldata)
         getRecom(alldata);
     })
-
 }
 
 var getRecom = function(array) {
-    console.log(array)
     var promises = array.map( (id) => {
         return new Promise(function(res) {
-
             var recurl = "https://api.themoviedb.org/3/movie/" + String(id)  +"/recommendations?api_key=c5e11b07aed33fed93509604abbe325f&language=en-US&page=1"
             var recommends = [];
-    
             $.ajax({
                 url: recurl,
                 method: "GET"
@@ -57,54 +45,31 @@ var getRecom = function(array) {
                     recommends.push(tempid)
                 }        
                 recomObj[id] = recommends;
-                console.log(recommends)
-
-
                 res(recomObj)
-                
             });
-
-
         })
     })
-
     Promise.all(promises).then(function(alldata) {
-        console.log(alldata);
-        console.log(recomObj)
+        sortObj(recomObj);
     })
-
 }
 
 var sortObj = function(myobject) {
     objKeys = Object.keys(myobject);
+    var ratedMovies = [];
     for (var i = 0; i < objKeys.length; i++) {
         for (var j = 0; j < myobject[objKeys[i]].length; j++) {
             var movie = myobject[objKeys[i]][j];
             
             if (ratedMovies.indexOf(movie) < 0) {
                 var rating = getRated(myobject,movie);
-                ratedMovies.append(movie);
+                ratedMovies.push(movie);
                 ratedObj[movie] = rating;
             }
         }
     }
-}
-
-var getIMDBids = function(array) {
-    
-    for (var id = 0; id < array.length; id++) {
-        var idurl = "https://api.themoviedb.org/3/movie/" + array[id] + " ?api_key=c5e11b07aed33fed93509604abbe325f&language=en-US"
-        var imdbID = "";
-        var recommends = [];
-        $.ajax({
-            url: idurl,
-            method: "GET"
-        }).then(function(data) {
-            imdbid = data.val().imdb_id;
-            recommends.push(imdbid);
-        });
-    }
-    return recommends;
+    sortRatings(ratedObj);
+    getIMDBids(recommendArray)
 }
 
 var getRated = function(movieObj, movie) {
@@ -124,25 +89,46 @@ var sortRatings = function(myobject) {
         sortable.push([id, myobject[id]]);
     }
     sortable.sort(function(a, b) {
-        return a[1] - b[1];
+        return b[1] - a[1];
     })
 
-    for (var i = sortable.length-1; i>=0;i--){
-        recommendation.push(sortable[i][0])
+    for (var i = 0;  i< sortable.length;i++){
+        recommendArray.push(sortable[i][0])
     }
-    return sortable;
+}
+
+var getIMDBids = function(array) {
+    var recommends = [];
+    var promises = array.map((id) => {
+        return new Promise(function(res) { 
+            var idurl = "https://api.themoviedb.org/3/movie/" + id + " ?api_key=c5e11b07aed33fed93509604abbe325f&language=en-US"
+            var imdbID = "";
+            $.ajax({
+                url: idurl,
+                method: "GET"
+            }).then(function(data) {
+                imdbid = data.imdb_id;
+                recommends.push(imdbid);
+                res(imdbid)
+            });
+        })
+    })
+    Promise.all(promises).then(function(alldata) {
+        recommendations = alldata;
+        console.log(recommendations);
+    })
 }
 
 
-console.log(imdbIDs)
-tmdbIDs = getTMDBids(imdbIDs);
 
-//console.log(recomObj)
-// sortObj(recomObj);
-// console.log(ratedObj)
-// recommendArray = sortRatings(ratedObj)
-// console.log(recommendArray)
-// recommendations = getIMDBids(recommendArray);
-// console.log(recommendations);
 
+var promise2 = new Promise(function(res) { 
+    Promise.resolve(getRecommendations(favmovies));
+    res()
+})
+
+promise2.then(function(val) {
+    console.log(recommendations);
+    console.log(recommendArray)
+})
 
