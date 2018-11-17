9c42a1bcc1de6415af13bdd449519e30673255c7
18eddymedina <18emedina@gmail.com>
2018-11-17 11:18:28 -0600

    added the click for favorites

--- app.js
+++ app.js
@@ -1,22 +1,10 @@
 
-
 var arr=[];
- // arr.push(temp);
 
 $("#search").on("click", function(){
 
     var movie = $("#q").val();
     var url = "https://www.omdbapi.com/?s=" + movie + "&plot=short&apikey=6cf3f906";
-
-// $.ajax({
-//     url: url,
-//     method: "GET"
-// }).then(function(response) {
-
-//   console.log(response.Poster);
-// });
-
-
 $.ajax({
     type: "GET",
     url: url,
@@ -24,19 +12,35 @@ $.ajax({
     for(var i=1; i<10;i++){
         var posterDiv = $("<div>");
 
-        //console.log(res.Search[i].Title);
         var pic = $("<img>");
+        pic.addClass("pic")
         pic.attr("src", res.Search[i].Poster);    
-        pic.attr("style","height: 100px");
         var p = $("<p>").text(res.Search[i].Title);
         posterDiv.prepend(p);
         posterDiv.append(pic);
+        posterDiv.attr("value",res.Search[i].imdbID);
+
+        posterDiv.data(res.Search[i].imdbID);
+        posterDiv.addClass("picture")
         posterDiv.attr("style","display: inline-block");
-
-        $("#posters").prepend(posterDiv);
-
+ 
+        
+        // //posters is where the posterDIv will prepend
+         $("#posters").prepend(posterDiv);
+        // //posterDiv is where the picture and title will be displayd inline block.
     }
+    $(".picture").on("click", function(){
+        var val=this.getAttribute("value");
+        if(arr.includes(val)){
+            arr=arr.filter(arr=>arr!==val);
+        }
+        else{
+            arr.push(val);        
 
+        }
+
+
+  })
 
 });
 
